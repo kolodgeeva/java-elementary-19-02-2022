ae45acace1717be8f6f54fdd22f0bf6c35626542
Mariana Kolodzheieva <os-mari.kolodzheieva@rakuten.com>
2022-02-19 14:01:18 +0200

    Add lesson 1

---
+++ src/com/hillel/classwork/lesson1/For.java
@@ -0,0 +1,23 @@
+package com.hillel.classwork.lesson1;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        //i++;
+        //i = i + 1;
+
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Я супер программист");
+        }
+
+        //i--;
+        //i = i - 1;
+
+        // print numbers from 100 to 1
+        for (int i = 100; i >= 0 ; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
